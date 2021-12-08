f7d2f924da29508907649d4a12d6c3ce1427db3d
Hansu Kim <wilson.bear@kakaobrain.com>
2021-12-08 22:50:24 +0900

    fin

--- routes/home.js
+++ routes/home.js
@@ -7,6 +7,14 @@ const { getPostsFromQuery } = require('../utils');
 const router = express.Router();
 
 router.use(async (req, res, next) => {
+  res.locals.user = req.user;
+  if (!req.query['page']) {
+    res.locals.page_num = 1;
+  } else {
+    res.locals.page_num = req.query['page'];
+  }
+  console.log(res.locals.page_num);
+
   res.locals.posts = await getPostsFromQuery(req.query);
   next();
 });
